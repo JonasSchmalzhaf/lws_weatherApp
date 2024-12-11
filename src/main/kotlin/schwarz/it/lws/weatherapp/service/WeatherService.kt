aaba32cbcb9b40e20e437793dfc1a099b9746229
JonasSchmalzhaf <jonas.schmalzhaf@stackit.cloud>
2024-12-11 15:19:52 +0100

    Updated WeatherService.kt make City case safety; Updated WeatherService.kt make description and icon save for day/night

--- src/main/kotlin/schwarz/it/lws/weatherapp/service/WeatherService.kt
+++ src/main/kotlin/schwarz/it/lws/weatherapp/service/WeatherService.kt
@@ -10,19 +10,22 @@ import java.math.BigDecimal
 import java.math.RoundingMode
 import java.time.LocalDate
 import java.time.LocalDateTime
+import java.util.*
 
 @Service
 class WeatherService(private val weatherRepository: WeatherRepository, private val restTemplateBuilder: RestTemplateBuilder) {
     fun getWeatherForecast(city: String, days: Int): List<WeatherData> {
-        if (weatherRepository.findByCity(city).isEmpty()) {
-            getForecastFromOpenWeatherMap(city)
+        val safeCity = city.lowercase().replaceFirstChar { if (it.isLowerCase()) it.titlecase(Locale.getDefault()) else it.toString() }
+
+        if (weatherRepository.findByCity(safeCity).isEmpty()) {
+            getForecastFromOpenWeatherMap(safeCity)
         }
 
-        if (weatherRepository.findByCity(city).filter { LocalDate.now() <= it.forecastDate && it.forecastDate <= LocalDate.now().plusDays(days.toLong()) }.size < days) {
+        if (weatherRepository.findByCity(safeCity).filter { LocalDate.now() <= it.forecastDate && it.forecastDate <= LocalDate.now().plusDays(days.toLong()) }.size < days) {
             refreshWeatherForecastData()
         }
 
-        return weatherRepository.findByCity(city).filter { LocalDate.now() <= it.forecastDate && it.forecastDate <= LocalDate.now().plusDays(days.toLong()) }
+        return weatherRepository.findByCity(safeCity).filter { LocalDate.now() <= it.forecastDate && it.forecastDate <= LocalDate.now().plusDays(days.toLong()) }
     }
 
     @Scheduled(cron = "0 * /3 * * *")
@@ -38,12 +41,18 @@ class WeatherService(private val weatherRepository: WeatherRepository, private v
         val apiKey = "0a796a8566000abaaf2677586ccb7571"
         val uri = "https://api.openweathermap.org/data/2.5/forecast?q=$city&appid=$apiKey&units=metric"
 
-        val openWeatherMap = restTemplateBuilder.build()
-        val receivedForecast = openWeatherMap.getForObject(uri, WeatherForecastModel.WeatherForecast::class.java)
+        try {
+            val openWeatherMap = restTemplateBuilder.build()
+            val receivedForecast = openWeatherMap.getForObject(uri, WeatherForecastModel.WeatherForecast::class.java)
 
-        weatherRepository.findByCity(city).filter { it.forecastDate > LocalDate.now().minusDays(1) }.forEach { weather -> weatherRepository.deleteById(weather.id!!) }
+            weatherRepository.findByCity(city).filter { it.forecastDate > LocalDate.now().minusDays(1) }.forEach { weather -> weatherRepository.deleteById(weather.id!!) }
 
-        forecastToDaily(receivedForecast!!).forEach { weather -> weatherRepository.save(weather) }
+            forecastToDaily(receivedForecast!!).forEach { weather ->
+                weatherRepository.save(weather)
+            }
+        } catch (error: Exception) {
+            println(error.message);
+        }
     }
 
     fun forecastToDaily(forecast: WeatherForecastModel.WeatherForecast): List<WeatherData> {
@@ -57,8 +66,8 @@ class WeatherService(private val weatherRepository: WeatherRepository, private v
             minTemperatur = BigDecimal(dayForecast.minOf { it.main.temp_min }).setScale(1, RoundingMode.HALF_EVEN).toDouble(),
             maxTemperature = BigDecimal(dayForecast.maxOf { it.main.temp_max }).setScale(1, RoundingMode.HALF_EVEN).toDouble(),
             humidity = dayForecast.sumOf { it.main.humidity } / dayForecast.size,
-            description = dayForecast.filter { it.weather.first().icon.contains("d") }.groupingBy { it.weather.first().description }.eachCount().maxBy { it.value }.key,
-            iconCode = dayForecast.filter { it.weather.first().icon.contains("d") }.groupingBy { it.weather.first().icon }.eachCount().maxBy { it.value }.key,
+            description = if (dayForecast.filter { it.weather.first().icon.contains("d") }.isEmpty())  dayForecast.groupingBy { it.weather.first().description }.eachCount().maxBy { it.value }.key else dayForecast.filter { it.weather.first().icon.contains("d") }.groupingBy { it.weather.first().description }.eachCount().maxBy { it.value }.key,
+            iconCode = if(dayForecast.filter { it.weather.first().icon.contains("d") }.isEmpty()) dayForecast.groupingBy { it.weather.first().icon }.eachCount().maxBy { it.value }.key else dayForecast.filter { it.weather.first().icon.contains("d") }.groupingBy { it.weather.first().icon }.eachCount().maxBy { it.value }.key,
         ))}
 
         return dailyForecast
